1da2ab8c191274fcbed7260dce64691d137b8832
Jeremy Myers <echelonical@gmail.com>
2022-08-12 13:43:14 -0500

    updated modules to add name of items to DOM

--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -12,11 +12,17 @@ export const JewelryStyles = () => {
     let html = "<ul>"
 
     // Use .map() for converting objects to <li> elements
-    const listItemsArray =
+    const listItemsArray = styles.map(
+        (style) => {
+            return `<li>
+            <input type="radio" name="styles" value="${style.id}"/>${style.style}
+            </li>`
+        }
+    )
 
 
-        // Join all of the strings in the array into a single string
-        html += listItems.join("")
+    // Join all of the strings in the array into a single string
+    html += listItemsArray.join("")
 
     html += "</ul>"
     return html
