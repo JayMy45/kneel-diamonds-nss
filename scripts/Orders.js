8afd4411d2675e9b1a2f629ddea6ebd3a12bf56f
Jeremy Myers <echelonical@gmail.com>
2022-08-15 20:10:23 -0500

    updated comments on Order.js module to describe code better

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,7 +1,6 @@
-import { getOrders, getMetals, getSizes, getStyles } from "./database.js"
+import { getMetals, getSizes, getStyles } from "./database.js"
 
 
-const order = getOrders()
 const metals = getMetals()
 const sizes = getSizes()
 const styles = getStyles()
@@ -10,10 +9,11 @@ const styles = getStyles()
 const buildOrderListItem = (order) => {
 
     // Remember that the function you pass to find() must return true/false
-    const foundMetal = metals.find(
-        (metal) => {
-            return metal.id === order.metalId
-        }
+    const foundMetal = metals.find( //dot.find method 
+        //this function must include foundMetal (et al) in buildOrderListItem function to work properly...this how it becomes synchronized with whatever is click
+        (metal) => {  //(metal) is the element of the dot method
+            return metal.id === order.metalId //this conditional(?) returns whichever comparison is truthy...looping(?) through each option until truthy which is then presented to the total variable
+        }  //what is returned is directly based off of the choice that was made by the user.
     )
 
     const foundSize = sizes.find(
@@ -38,7 +38,7 @@ const buildOrderListItem = (order) => {
 
     return `<li>
         Order #${order.id} was placed on ${costString}
-    </li>`
+    </li>`                                //{costString} updated the callback to record/print price result...
 }
 
 export const Orders = () => {
